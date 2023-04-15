af2d0535d22d660e1a70dbbfedc69b583c3dce21
Ayush <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-04-15 11:10:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,13 +15,13 @@ global.APIKeys = {
 }
 
 //language 
-global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+global.language = english //change indonesia to english if you don't understand the language used by the bot
 
 global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
 global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
+global.OWNER_NAME = "Ayush" //your name
+global.OWNER_NUMBER = "918602206506" //your telegram number
 global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
-global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
+global.THUMBNAIL = "./image/ayush.png" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.DONASI = "./image/nice.png" // foto donasi di folder image
 global.lang = language //don't change
